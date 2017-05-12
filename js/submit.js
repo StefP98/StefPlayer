add2e1eb1c6d6f02af13ecba59bd738eaf9ac410
Stefan Pop <stefan.popd@gmail.com>
2017-05-12 08:22:58 +0300

    update 12/05/2017 8:22

--- js/submit.js
+++ js/submit.js
@@ -68,3 +68,103 @@ function ver6(){
 </div>`;
 document.getElementById("oldvers6").innerHTML = inHtml;
 }
+function ver7(){
+	var vers = document.getElementById("title7").innerHTML.toUpperCase() + "<br><br>" + document.getElementById("textarea7").value;
+	var vers2 = vers.replace(/\n/g,"<br>");
+	var inHtml = `<div class="col s12"><div class="card-panel">
+<span class="teal-text">${vers2}
+</span>
+</div>
+</div>`;
+document.getElementById("oldvers7").innerHTML = inHtml;
+}
+function ver8(){
+	var vers = document.getElementById("title8").innerHTML.toUpperCase() + "<br><br>" + document.getElementById("textarea8").value;
+	var vers2 = vers.replace(/\n/g,"<br>");
+	var inHtml = `<div class="col s12"><div class="card-panel">
+<span class="teal-text">${vers2}
+</span>
+</div>
+</div>`;
+document.getElementById("oldvers8").innerHTML = inHtml;
+}
+function ver9(){
+	var vers = document.getElementById("title9").innerHTML.toUpperCase() + "<br><br>" + document.getElementById("textarea9").value;
+	var vers2 = vers.replace(/\n/g,"<br>");
+	var inHtml = `<div class="col s12"><div class="card-panel">
+<span class="teal-text">${vers2}
+</span>
+</div>
+</div>`;
+document.getElementById("oldvers9").innerHTML = inHtml;
+}
+function ver10(){
+	var vers = document.getElementById("title10").innerHTML.toUpperCase() + "<br><br>" + document.getElementById("textarea10").value;
+	var vers2 = vers.replace(/\n/g,"<br>");
+	var inHtml = `<div class="col s12"><div class="card-panel">
+<span class="teal-text">${vers2}
+</span>
+</div>
+</div>`;
+document.getElementById("oldvers10").innerHTML = inHtml;
+}
+function ver11(){
+	var vers = document.getElementById("title11").innerHTML.toUpperCase() + "<br><br>" + document.getElementById("textarea11").value;
+	var vers2 = vers.replace(/\n/g,"<br>");
+	var inHtml = `<div class="col s12"><div class="card-panel">
+<span class="teal-text">${vers2}
+</span>
+</div>
+</div>`;
+document.getElementById("oldvers11").innerHTML = inHtml;
+}
+function ver12(){
+	var vers = document.getElementById("title12").innerHTML.toUpperCase() + "<br><br>" + document.getElementById("textarea12").value;
+	var vers2 = vers.replace(/\n/g,"<br>");
+	var inHtml = `<div class="col s12"><div class="card-panel">
+<span class="teal-text">${vers2}
+</span>
+</div>
+</div>`;
+document.getElementById("oldvers12").innerHTML = inHtml;
+}
+function ver13(){
+	var vers = document.getElementById("title13").innerHTML.toUpperCase() + "<br><br>" + document.getElementById("textarea13").value;
+	var vers2 = vers.replace(/\n/g,"<br>");
+	var inHtml = `<div class="col s12"><div class="card-panel">
+<span class="teal-text">${vers2}
+</span>
+</div>
+</div>`;
+document.getElementById("oldvers13").innerHTML = inHtml;
+}
+function ver14(){
+	var vers = document.getElementById("title14").innerHTML.toUpperCase() + "<br><br>" + document.getElementById("textarea14").value;
+	var vers2 = vers.replace(/\n/g,"<br>");
+	var inHtml = `<div class="col s12"><div class="card-panel">
+<span class="teal-text">${vers2}
+</span>
+</div>
+</div>`;
+document.getElementById("oldvers14").innerHTML = inHtml;
+}
+function ver15(){
+	var vers = document.getElementById("title15").innerHTML.toUpperCase() + "<br><br>" + document.getElementById("textarea15").value;
+	var vers2 = vers.replace(/\n/g,"<br>");
+	var inHtml = `<div class="col s12"><div class="card-panel">
+<span class="teal-text">${vers2}
+</span>
+</div>
+</div>`;
+document.getElementById("oldvers15").innerHTML = inHtml;
+}
+function ver16(){
+	var vers = document.getElementById("title16").innerHTML.toUpperCase() + "<br><br>" + document.getElementById("textarea16").value;
+	var vers2 = vers.replace(/\n/g,"<br>");
+	var inHtml = `<div class="col s12"><div class="card-panel">
+<span class="teal-text">${vers2}
+</span>
+</div>
+</div>`;
+document.getElementById("oldvers16").innerHTML = inHtml;
+}
